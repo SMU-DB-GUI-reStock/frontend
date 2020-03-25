8a11b82b9f2d93dc2b4860d19664269230a92fe2
lizzyhanna <lhanna@smu.edu>
2020-03-24 19:09:37 -0500

    refershed commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-    
         <img src="https://amznsellertools.com/wp-content/uploads/Restock-Pro-Logo.png"></img>
         <h2>Log in</h2>
-        <label for="username">Username</label>
+        <label HTMLfor="username">Username</label>
         <input name="username" type="text"></input>
-        <label for="password">Password</label>
+        <label HTMLfor="password">Password</label>
         <input name="password" type="password"></input>
         <button id="logIn">Log In</button>
         <button id="forgotPassword">Forgot Password?</button>
-    
       </header>
     </div>
   );
