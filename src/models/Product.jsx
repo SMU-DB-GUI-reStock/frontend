c73eb004943fb8a33ec5ee259348a3769fc5aa50
lizzyhanna <lhanna@smu.edu>
2020-04-04 21:57:43 -0500

    added models

---
+++ src/models/Product.jsx
@@ -0,0 +1,7 @@
+export class Product{
+    constructor(product_id, dept_id, product_name){
+        this.produt_id = product_id;
+        this.dept_id = dept_id
+        this.product_name = product_name;
+    }
+}
