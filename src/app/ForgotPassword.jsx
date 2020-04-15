9caa3ca1d5bdbb51a0519343277ad3379b198315
lizzyhanna <lhanna@smu.edu>
2020-04-14 19:37:56 -0500

    cleaned up forgotpassword.css

--- src/app/ForgotPassword.jsx
+++ src/app/ForgotPassword.jsx
@@ -7,13 +7,13 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 export class ForgotPassword extends React.Component{
     render(){
       return (
-        <div className="contain" style={{background: '#50E3C2', height: '47.4rem'}}>
-            <h1>Forget your password?</h1>
-            <h4>Please get in contact with the administrator: admin@gmail.com</h4>
+        <div className="contain jumbotron">
+            <h1 className="display-4">Forget your password?</h1>
+            <p className="lead">Please get in contact with the administrator: admin@gmail.com</p>
             <Link to='/'>
                 <button id="forgotPassword" 
                     className="btn btn-secondary" >
-                    Login
+                    Back to Login
                 </button>
             </Link>
         </div>
